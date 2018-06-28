179012187936f5202791633009a2a87fcfa3a3d6
BradHick <bradleyhrm@gmail.com>
2018-06-28 20:56:59 -0300

    Initial commit

---
+++ src/config/routes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const auth = require('./auth');
+
+module.exports = function(server) {
+  
+  /**
+  |--------------------------------------------------
+  | Protected Routes with JWT Token
+  |--------------------------------------------------
+  */
+
+  const protectedApi = express.Router();
+  server.use('/api', protectedApi);
+
+  protectedApi.use(auth);
+
+  const BillingCycle = require('../api/billingCycle/billingCycleService');
+  BillingCycle.register(protectedApi, '/billingCycles');
+
+
+  /**
+  |--------------------------------------------------
+  | Open routes
+  |--------------------------------------------------
+  */
+
+  const openApi = express.Router();
+  server.use('/oapi', openApi);
+
+  const AuthService = require('../api/user/authService');
+  openApi.post('/login', AuthService.login);
+  openApi.post('/signup', AuthService.signUp);
+  openApi.post('/validateToken', AuthService.validateToken);
+  
+}
